84ade3daf635e77762bcd439b0aff86ae1f7e2c9
tom125813 <thomasrayner16@gmail.com>
2022-11-06 11:22:32 +0000

    Initial commit

---
+++ src/main/java/com/doontcare/customrecipes/CustomRecipes.java
@@ -0,0 +1,19 @@
+package com.doontcare.customrecipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomRecipes extends JavaPlugin {
+
+    // TODO: Add an update checker which alerts staff and console on join.
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
